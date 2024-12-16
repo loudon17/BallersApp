34f9e9a973c93837f5dc6265c50a84e8c69eba8b
luigi <luigidonnino@gmail.com>
2024-12-16 12:13:33 +0100

    initial commit

---
+++ BallersApp/BallersApp/BallersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallersAppApp.swift
+//  BallersApp
+//
+//  Created by Luigi Donnino on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BallersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BallersApp/BallersAppTests/BallersAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BallersAppTests.swift
+//  BallersAppTests
+//
+//  Created by Luigi Donnino on 16/12/24.
+//
+
+import Testing
+@testable import BallersApp
+
+struct BallersAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
