2d715da8618e88ffdc287badddae91beacf503d3
luigi <luigidonnino@gmail.com>
2024-12-17 09:45:10 +0100

    working playerview that only shows one player instead of a list

---
+++ BallersApp/BallersApp/PlayerListView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerListView.swift
+//  BallersApp
+//
+//  Created by Luigi Donnino on 16/12/24.
+//
+
+import SwiftUI
+
+struct PlayerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerListView()
+}
---
+++ BallersApp/BallersApp/PlayerViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerViewModel.swift
+//  BallersApp
+//
+//  Created by Luigi Donnino on 16/12/24.
+//
+
+import SwiftUI
+
+struct PlayerViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerViewModel()
+}
