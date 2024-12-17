ae9076173583905d1b8614b258c0aaad76dd8cd8
luigi <luigidonnino@gmail.com>
2024-12-17 11:06:14 +0100

    details improved

---
+++ BallersApp/BallersApp/PlayerDetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerDetailViewModel.swift
+//  BallersApp
+//
+//  Created by Luigi Donnino on 17/12/24.
+//
+
+import SwiftUI
+
+struct PlayerDetailViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerDetailViewModel()
+}
---
+++ BallersApp/BallersApp/PlayerdetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerdetailView.swift
+//  BallersApp
+//
+//  Created by Luigi Donnino on 17/12/24.
+//
+
+import SwiftUI
+
+struct PlayerdetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerdetailView()
+}
