663184712c94446f7ff2596f6e57f9bc29885512
luigi <luigidonnino@gmail.com>
2024-12-18 10:36:03 +0100

    final stage

---
+++ BallersApp/BallersApp/FavouriteListView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouriteListView.swift
+//  BallersApp
+//
+//  Created by Luigi Donnino on 18/12/24.
+//
+
+import SwiftUI
+
+struct FavouriteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouriteListView()
+}
